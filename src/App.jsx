bb80ac0521902d915886dac8fb497ac954245d64
ChrisKim <1515748548@qq.com>
2024-04-22 16:40:56 +0800

    [feat] login, register 不显示侧栏

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import PageRouter from './router/PageRouter';
-import Layout, { Content, Footer, Header } from 'antd/es/layout/layout';
+import Layout, { Content, Header } from 'antd/es/layout/layout';
 import Sider from 'antd/es/layout/Sider';
 import Navbar from './component/Navbar';
 import SideMenu from './component/SideMenu';
+import { useLocation } from 'react-router-dom';
 
 const App = () => {
+  const { pathname } = useLocation();
+
+  const hideSideMenu = () => {
+    const includePaths = ['/login', '/register'];
+    for (let path of includePaths) {
+      if (pathname.startsWith(path)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   const headerStyle = {
     padding: "0",
     backgroundColor: "rgba(255, 255, 255, 0)",
@@ -19,15 +32,13 @@ const App = () => {
           <Navbar />
         </Header>
         <Layout>
-          <Sider width="256px" style={{ backgroundColor: "white" }}>
+          <Sider width="256px" style={{ backgroundColor: "white" }} hidden={hideSideMenu()}>
             <SideMenu />
           </Sider>
           <Content style={{ minHeight: "90vh" }}>
             <PageRouter />
           </Content>
         </Layout>
-        <Footer>
-        </Footer>
       </Layout>
     </div>
   );
