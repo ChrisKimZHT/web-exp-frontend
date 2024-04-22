e1584291adf8fa74527fe972c9933f2fa2c42c66
ChrisKim <1515748548@qq.com>
2024-04-22 19:46:26 +0800

    [feat] 日历

--- src/view/HomeView.jsx
+++ src/view/HomeView.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './HomeView.scss';
+import { Calendar } from 'antd';
 
 const HomeView = () => {
   return (
     <div className='home-view'>
       <div className='title'>
-        Home View
+        主页
+      </div>
+      <div className='calender-container'>
+        <Calendar />
       </div>
     </div>
   );
