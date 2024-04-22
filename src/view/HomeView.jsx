3b37021a57a413e269013ba9907af99e1ab48133
ChrisKim <1515748548@qq.com>
2024-04-22 20:54:31 +0800

    [feat] 首页显示便签

--- src/view/HomeView.jsx
+++ src/view/HomeView.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import './HomeView.scss';
-import { Calendar } from 'antd';
+import { Calendar, Col, Row } from 'antd';
 
 const HomeView = () => {
   return (
     <div className='home-view'>
-      <div className='title'>
-        主页
-      </div>
-      <div className='calender-container'>
-        <Calendar />
-      </div>
+      <Row className='row'>
+        <Col lg={8} className='col left'>
+          <div className='title'>
+            便签
+          </div>
+          <div className='note-container'>
+            awa
+          </div>
+        </Col>
+        <Col lg={16} className='col right'>
+          <div className='title'>
+            待办事项
+          </div>
+          <div className='calender-container'>
+            <Calendar />
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
