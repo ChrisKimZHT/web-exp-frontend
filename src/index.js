e723e0325b59ae3315124d2197de0466d0624234
ChrisKim <1515748548@qq.com>
2024-04-24 16:54:24 +0800

    [feat] dayjs指定中文

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
+import 'dayjs/locale/zh-cn';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
