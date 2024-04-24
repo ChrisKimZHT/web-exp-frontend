113158121e31309407fd1bd81210243f13f8aadb
ChrisKim <1515748548@qq.com>
2024-04-24 16:21:36 +0800

    [refactor] 把App组件样式分离

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,14 @@
 import React from 'react';
+import './App.scss';
 import PageRouter from './router/PageRouter';
-import Layout, { Content, Header } from 'antd/es/layout/layout';
-import Sider from 'antd/es/layout/Sider';
+import Layout, { Header } from 'antd/es/layout/layout';
 import Navbar from './component/Navbar';
-import SideMenu from './component/SideMenu';
-import { useLocation } from 'react-router-dom';
 
 const App = () => {
-  const { pathname } = useLocation();
-
-  const hideSideMenu = () => {
-    const includePaths = ['/login', '/register', '/forget-password'];
-    for (let path of includePaths) {
-      if (pathname.startsWith(path)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  const headerStyle = {
-    padding: "0",
-    backgroundColor: "rgba(255, 255, 255, 0)",
-    zIndex: "1000",
-  }
-
-  window.addEventListener('resize', () => {
-    if (window.innerWidth > 768) {
-      document.querySelector('.ant-layout-sider').style.display = 'block';
-    } else {
-      document.querySelector('.ant-layout-sider').style.display = 'none';
-    }
-  });
-
   return (
     <div className='app'>
       <Layout>
-        <Header style={headerStyle}>
+        <Header className='header'>
           <Navbar />
         </Header>
         <PageRouter />
