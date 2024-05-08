f979e233de87fa2ea8bb223be65eb175aef6e1f4
ChrisKim <1515748548@qq.com>
2024-05-08 17:02:46 +0800

    [feat] 支持从navbar跳转个人中心

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -24,7 +24,8 @@ const Navbar = ({ isDarkTheme, setIsDarkTheme }) => {
         </>
       ) : (
         <>
-          <div className='item'>{localStorage.getItem('email').length > 12 ? localStorage.getItem('email').substring(0, 12) + "..." : localStorage.getItem('email')}</div>
+          <div className='item'>{localStorage.getItem('email').length > 9 ? localStorage.getItem('email').substring(0, 9) + "..." : localStorage.getItem('email')}</div>
+          <div className='item clickable' onClick={() => navigate("/user")}><UserOutlined />&nbsp;个人中心</div>
           <div className='item clickable' onClick={onLogout}><LogoutOutlined />&nbsp;退出登录</div>
         </>
       )}
