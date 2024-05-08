246cfcdde04bc5521a7a03802381b45b9c7ef79b
ChrisKim <1515748548@qq.com>
2024-05-08 11:51:18 +0800

    [feat] note表格编辑功能

--- src/view/NoteView.jsx
+++ src/view/NoteView.jsx
@@ -1,212 +1,206 @@
-import React, { useState } from 'react';
-import { Button, Table } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { App, Form, Table, Tag, Typography, Popconfirm, InputNumber, Input, Switch } from 'antd';
 import './NoteView.scss';
 import dayjs from 'dayjs';
 import { SnippetsOutlined } from '@ant-design/icons';
+import service from '../service/service';
 
-const sampleData = [
-  {
-    no: 1,
-    title: '标题1',
-    content: '内容1',
-    time: 1630416000
-  },
-  {
-    no: 2,
-    title: '标题2',
-    content: '内容2',
-    time: 1630416000
-  },
-  {
-    no: 3,
-    title: '标题3',
-    content: '内容3',
-    time: 1630416000
-  },
-  {
-    no: 4,
-    title: '标题4',
-    content: '内容4',
-    time: 1630416000
-  },
-  {
-    no: 5,
-    title: '标题5',
-    content: '内容5',
-    time: 1630416000
-  },
-  {
-    no: 6,
-    title: '标题6',
-    content: '内容6',
-    time: 1630416000
-  },
-  {
-    no: 7,
-    title: '标题7',
-    content: '内容7',
-    time: 1630416000
-  },
-  {
-    no: 8,
-    title: '标题8',
-    content: '内容8',
-    time: 1630416000
-  },
-  {
-    no: 9,
-    title: '标题9',
-    content: '内容9',
-    time: 1630416000
-  },
-  {
-    no: 10,
-    title: '标题10',
-    content: '内容10',
-    time: 1630416000
-  },
-  {
-    no: 11,
-    title: '标题11',
-    content: '内容11',
-    time: 1630416000
-  },
-  {
-    no: 12,
-    title: '标题12',
-    content: '内容12',
-    time: 1630416000
-  },
-  {
-    no: 13,
-    title: '标题13',
-    content: '内容13',
-    time: 1630416000
-  },
-  {
-    no: 14,
-    title: '标题14',
-    content: '内容14',
-    time: 1630416000
-  },
-  {
-    no: 15,
-    title: '标题15',
-    content: '内容15',
-    time: 1630416000
-  },
-  {
-    no: 16,
-    title: '标题16',
-    content: '内容16',
-    time: 1630416000
-  },
-  {
-    no: 17,
-    title: '标题17',
-    content: '内容17',
-    time: 1630416000
-  },
-  {
-    no: 18,
-    title: '标题18',
-    content: '内容18',
-    time: 1630416000
-  },
-  {
-    no: 19,
-    title: '标题19',
-    content: '内容19',
-    time: 1630416000
-  },
-  {
-    no: 20,
-    title: '标题20',
-    content: '内容20',
-    time: 1630416000
-  },
-  {
-    no: 21,
-    title: '标题21',
-    content: '内容21',
-    time: 1630416000
-  },
-  {
-    no: 22,
-    title: '标题22',
-    content: '内容22',
-    time: 1630416000
-  },
-  {
-    no: 23,
-    title: '标题23',
-    content: '内容23',
-    time: 1630416000
-  },
-  {
-    no: 24,
-    title: '标题24',
-    content: '内容24',
-    time: 1630416000
-  },
-  {
-    no: 25,
-    title: '标题25',
-    content: '内容25',
-    time: 1630416000
-  },
-]
-
+const EditableCell = ({
+  editing,
+  dataIndex,
+  title,
+  inputType,
+  record,
+  index,
+  children,
+  ...restProps
+}) => {
+  const nodeMap = {
+    'number': <InputNumber />,
+    'text': <Input />,
+    'boolean': <Switch />
+  }
+  const inputNode = nodeMap[inputType];
+  return (
+    <td {...restProps}>
+      {editing ? (
+        <Form.Item
+          name={dataIndex}
+          style={{
+            margin: 0,
+          }}
+          rules={[
+            {
+              required: true,
+              message: `Please Input ${title}!`,
+            },
+          ]}
+        >
+          {inputNode}
+        </Form.Item>
+      ) : (
+        children
+      )}
+    </td>
+  );
+};
 
 const NoteView = () => {
-  const [tableData, setTableData] = useState(sampleData)
+  const [form] = Form.useForm();
+  const [tableData, setTableData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [editingKey, setEditingKey] = useState('');
+  const { message } = App.useApp();
+
+  const editRow = (record) => {
+    form.setFieldsValue({
+      ...record,
+    });
+    setEditingKey(record.noteId);
+  };
+
+  const saveRow = (record) => {
+    form.validateFields().then(values => {
+      const { title, content, isStared } = values;
+      service.note.update(record.noteId, title, content, record.date, isStared).then(res => {
+        message.success('保存成功');
+        setEditingKey('');
+        refreshData();
+      }).catch(err => {
+        message.error('保存失败');
+      })
+    }).catch(err => {
+      message.error('请填写完整信息');
+    })
+  }
 
   const columns = [
     {
       title: '#',
-      dataIndex: 'no',
-      key: 'no',
-      width: 100
+      dataIndex: 'noteId',
+      key: 'noteId',
+      width: 60
+    },
+    {
+      title: '星标',
+      dataIndex: 'isStared',
+      key: 'isStared',
+      render: (isStared) => isStared ? <Tag color='gold'>已标</Tag> : <Tag>未标</Tag>,
+      width: 80,
+      editable: true,
+      editType: 'boolean'
     },
     {
       title: '标题',
       dataIndex: 'title',
       key: 'title',
+      editable: true,
+      editType: 'text'
     },
     {
       title: '内容',
       dataIndex: 'content',
       key: 'content',
+      editable: true,
+      editType: 'text'
     },
     {
       title: '创建时间',
-      dataIndex: 'time',
-      key: 'time',
+      dataIndex: 'date',
+      key: 'date',
       render: (time) => dayjs.unix(time).format('YYYY-MM-DD HH:mm:ss'),
       width: 160
     },
     {
       title: '操作',
       key: 'action',
-      render: (record) => (
-        <Button size='small' danger={true} onClick={() => { console.log(record["no"]) }}>
-          删除
-        </Button>
-      ),
+      render: (_, record) => {
+        return record.noteId === editingKey ? (
+          <span>
+            <Typography.Link
+              onClick={() => saveRow(record)}
+              style={{
+                marginRight: 8,
+              }}
+            >
+              保存
+            </Typography.Link>
+            <Popconfirm title="确认放弃更改？" onConfirm={() => setEditingKey('')}>
+              <a>取消</a>
+            </Popconfirm>
+          </span>
+        ) : (
+          <Typography.Link disabled={editingKey !== ''} onClick={() => editRow(record)}>
+            编辑
+          </Typography.Link>
+        );
+      },
     },
   ]
+
+  const refreshData = () => {
+    setIsLoading(true);
+    service.note.list().then(res => {
+      const originalData = res.data.data;
+      const data = originalData.map((item, index) => {
+        const { noteId, title, content, date, isStared } = item;
+        return {
+          key: index + 1,
+          noteId,
+          title,
+          content,
+          date,
+          isStared
+        }
+      })
+      setTableData(data);
+      setIsLoading(false);
+    }).catch(err => {
+      message.error('获取便签列表失败');
+    })
+  }
+
+  const mergedColumns = columns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        inputType: col.editType,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editing: record.noteId === editingKey,
+      }),
+    };
+  });
+
+  // eslint-disable-next-line
+  useEffect(() => { refreshData() }, [])
 
   return (
     <div className='note-view'>
       <div className='title'>
         <SnippetsOutlined /> 便签
       </div>
-      <Table
-        columns={columns}
-        dataSource={tableData}
-        bordered={true}
-        pagination={{ position: ["bottomCenter"] }}
-      />
+      <Form form={form} component={false}>
+        <Table
+          components={{
+            body: {
+              cell: EditableCell,
+            },
+          }}
+          loading={isLoading}
+          columns={mergedColumns}
+          dataSource={tableData}
+          bordered={true}
+          pagination={{
+            position: ["bottomCenter"],
+            onChange: () => { setEditingKey('') },
+          }}
+        />
+      </Form>
     </div>
   );
 }
